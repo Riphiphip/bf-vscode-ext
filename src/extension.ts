f1db23138f5bde8620700d451295ddbcad58dc79
riphiphip <43417915+Riphiphip@users.noreply.github.com>
2022-07-28 22:56:25 +0200

    Activate extension + set up TypeScript

---
+++ src/extension.ts
@@ -0,0 +1,6 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+	// Register our custom editor providers
+    console.log("Brainfuck language extension activated");
+}
